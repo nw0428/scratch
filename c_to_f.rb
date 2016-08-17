56e33e309d6530b6238bcf37a5b684c6bef16efa
David Zabner <david@cazena.com>
2016-08-16 20:37:10 -0400

    minor changes

--- c_to_f.rb
+++ c_to_f.rb
@@ -6,9 +6,9 @@ def f_to_celcius f
   return (f - 32) * 5 / 9
 end
 
-# puts "Please enter a temperature in celcius"
-# celcius = gets.chomp.to_f
-# puts celcius.to_s + " in farenheight is: " + celcius_to_f(celcius).to_s
+puts "Please enter a temperature in celcius"
+celcius = gets.chomp.to_f
+puts celcius.to_s + " in farenheight is: " + celcius_to_f(celcius).to_s
 
 # This bit of the code does temperature conversion
 puts "Please enter a temperature in farenheight"

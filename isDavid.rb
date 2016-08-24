12d2846fccef473d884b2df23f84c46191026b79
David Zabner <david@cazena.com>
2016-08-23 21:01:02 -0400

    added week 2 example code

---
+++ isDavid.rb
@@ -0,0 +1,12 @@
+name = gets.chomp.capitalize
+
+if name == "David"
+  puts "my owner! YAY"
+elsif name > "David"
+  puts "you are my greater"
+else
+  puts "bad person"
+end
+
+
+puts "now this program is done"

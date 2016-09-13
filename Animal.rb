3f55faf7f8760b4924f76673f8de6c2f1389a06b
David Zabner <david@cazena.com>
2016-09-13 19:56:11 -0400

    added blackjack game

--- Animal.rb
+++ Animal.rb
@@ -2,8 +2,7 @@ class Animal
   attr_accessor :predator, :age, :species, :name
   attr_reader :dead
 
-
-  def initialize predator, age, species, name
+  def initialize(predator, age, species, name)
     @predator = predator
     @age = age
     @species = species
@@ -12,47 +11,45 @@ class Animal
   end
 
   def birthday
-    age = age + 1
+    @age += 1
   end
 
   def hunt
     if predator
-      puts "You hunt and kill!"
+      puts 'You hunt and kill!'
     else
       puts "You can't hunt! You are a vegetarian"
     end
   end
 
   def swim
-    if species == "bulldog"
+    if species == 'bulldog'
       dead = true
     else
-      puts "You went on a nice swim"
+      puts 'You went on a nice swim'
     end
   end
 
   def bite
     if age < 50
-      puts "Ouch"
+      puts 'Ouch'
     else
-      puts "You lost your teeth long ago"
+      puts 'You lost your teeth long ago'
     end
   end
 
-  def fight opponent
+  def fight(opponent)
     if opponent.dead
-      puts "You are fighting a corpse. Good job"
+      puts 'You are fighting a corpse. Good job'
+    elsif predator && opponent.predator
+      puts 'you fight to a stand still'
+    elsif !predator && opponent.predator
+      @dead = true
+      puts "You have been killed by your opponent who is a #{opponent.species}"
+    elsif predator && opponent.predator
+      puts 'You scared it off'
     else
-      if self.predator && opponent.predator
-        puts "you fight to a stand still"
-      elsif !self.predator && opponent.predator
-        @dead = true
-        puts "You have been killed by your opponent who is a #{opponent.species}"
-      elsif self.predator && opponent.predator
-        puts "You scared it off"
-      else
-        puts "nothing happens. You are both pacifists"
-      end
+      puts 'nothing happens. You are both pacifists'
     end
   end
 end
